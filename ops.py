4614544bf535f04e6130120c3057ad1f904fecd4
bruno vianna <bruno@pobox.com>
2020-02-05 09:37:56 +0100

    Update ops.py

--- ops.py
+++ ops.py
@@ -11,13 +11,13 @@ try:
   scalar_summary = tf.scalar_summary
   histogram_summary = tf.histogram_summary
   merge_summary = tf.merge_summary
-  SummaryWriter = tf.train.SummaryWriter
+  #SummaryWriter = tf.train.SummaryWriter
 except:
   image_summary = tf.summary.image
   scalar_summary = tf.summary.scalar
   histogram_summary = tf.summary.histogram
   merge_summary = tf.summary.merge
-  SummaryWriter = tf.summary.FileWriter
+  #SummaryWriter = tf.summary.FileWriter
 
 if "concat_v2" in dir(tf):
   def concat(tensors, axis, *args, **kwargs):
